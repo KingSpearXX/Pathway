ef0ba096fd3a2df634f0a3bf90bb3b48df6d9929
rex rayla <pathway@rxedev.com>
2024-05-09 17:37:56 +0800

    Expanded Universe

--- w3_rex_rayla.py
+++ w3_rex_rayla.py
@@ -3,75 +3,138 @@
 # Author: Rex Rayla
 # Git: https://github.com/KingSpearXX/Pathway
 # Branch: week3
+import os, time
 
-def choice(context, choice1, choice2):
+items = []
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def choice(context, choices):
     while True:
         print(context)
-        user_input = input(f"What do you choose? {choice1.upper()}/{choice2.upper()}:").lower()
-        if user_input == choice1:
-            return 1
-        elif user_input == choice2:
-            return 2
-        elif user_input == "fly":
-            print("You fly away from the game. *!@#$% there's a glitch in the Matrix!!!.")
+        print ("\nChoices: ")
+        choice_str = "[EXIT] [ITEMS] "
+        for choice in choices:
+            choice_str += f"[{choice.upper()}] "
+        print(f"{choice_str}")
+        user_input = input(f"\nWhat do you choose?: ").lower()
+        if user_input in choices:
+            return user_input
+        elif user_input == "exit":
+            clear()
+            print("Goodbye!")
             exit()
+        elif user_input == "items":
+            clear()
+            print("\nItems: ")
+            for item in items:
+                print(f"{item.upper()}")
+            input("\nPress any key to continue...")
+            clear()
         else:
-            print("Invalid choice. Please try again.")
+            print("Invalid choice, try again! \n")
+            time.sleep(0.5)
+            clear()
 
-# Door choice
-print("Welcome to the Adventure Game!")
-print("You are standing in a room with two doors.")
-door = choice("Which door do you want to enter?", "left", "right")
-if door == 1:
-    print("You enter the left door and find a chest full of gold.")
-else:
-    print("You enter the right door and find a chest of a sword and shield.")
-
-# Merchant choice
-print("You exit the room and find yourself in a forest.")
-print("You see a merchant selling potions.")
-buy = choice("Do you want to buy a potion?", "yes", "no")
-if buy == 1 and door == 1:
-    print("You buy a potion and drink it. You feel a surge of energy and gain 10 health.")
-elif buy == 1 and door == 2:
-    print("You have no gold to buy a potion. You leave the merchant and continue on your journey.")
-else:
-    print("You leave the merchant and continue on your journey.")
-
-# Dragon choice and key
-key = 0
-alive = 1
-print("You continue through the forest and see a dragon.")
-print("The dragon is sleeping.")
-attack = choice("Do you want to attack the dragon?", "yes", "no")
-if attack == 1 and door == 1:
-    print("You attack the dragon and it wakes up. You are burned to a crisp.")
-elif attack == 1 and door == 2:
-    print("You attack the dragon and it wakes up. You fight valiantly and defeat the dragon. You find a key to a door in the dragon's lair.")
-    key = 1
-else:
-    print("You leave the dragon alone and continue through the forest.")
-
-if attack == 1 and door == 1 and buy == 1:
-    print("You used the potion to heal yourself and continue on your journey. The dragon was impressed by your bravery and gives you a key to a door in the dragon's lair!")
-    key = 1
-elif attack == 1 and door == 1 and buy == 2:
-    print("You did not have a potion to heal yourself.")
-    alive = 0
-
-# Final choice
-if key == 1 and alive == 1:
-    print("You find the door the key unlocks and enter.")
-    print("You find a room with a chest full of treasure.")
-    print("Congratulations! You have completed the Adventure Game!")
-
-if key == 0 and alive == 1:
-    print("You did not find the key to the door. You are lost in the forest. Game Over.")
-
-if alive == 0:
-    print("You have perished in the game. Game Over.")
+def message(msg):
+    print(f"\n{msg}\n")
 
 
+                 
+# Start of the game
+clear()
+terrain = choice ("You are in a forest, you see a path to the mountain and a path to the river, where do you want to go?", ["mountain", "river"])
 
+# Terrain
+while True:
+    clear()
 
+    # Items in the terrain
+    if terrain == "mountain" and "potion" not in items:
+        message("You found a potion, along the way!")
+        items.append("potion")
+    if terrain == "river" and "sword" not in items:
+        message("You found a sword, along the way!")
+        items.append("sword")
 
+    if terrain == "mountain":
+        direction = choice("You are now in the mountain, You see two paths, one to the north, one to the right that leads to the river.", ["north", "right"])
+    if terrain == "river":
+        direction = choice("You are now in the river, You see two paths, one to the north, one to the left that leads to the mountain.", ["north", "left"])
+    if direction == "north":
+        break
+    elif direction == "left":
+        terrain = "mountain"
+    elif direction == "right":
+        terrain = "river"
+
+# Event
+event_action = ""
+while True:
+    clear()
+    if terrain == "mountain":
+        event_action = choice("You see an old man stuck in a broken carriage, What would you like to do?", ["help", "ignore", "potion"])
+
+    if event_action == "help":
+        clear()
+        message("You helped the old man, but he still died from his wounds. You proceed to the north.")
+        event_action = "north"
+        break
+    elif event_action == "potion" and "potion" in items:
+        clear()
+        message("You helped and used the potion to heal the old man, in return he gave you a key. You proceed to the west.")
+        items.remove("potion")
+        items.append("key")
+        event_action = "west"
+        break
+    elif event_action == "potion" and "potion" not in items:
+        clear()
+        message("You don't have any potion to use!")
+        input("\nPress any key to continue...")
+    elif event_action == "ignore":
+        clear()
+        message("You ignored the old man and headed east.")
+        terrain = "river" 
+        input("\nPress any key to continue...")
+        clear()
+    
+    if terrain == "river":
+        event_action = choice("You see a barbarian king, What would you like to do?", ["fight", "run", "surrender"])
+    
+    if event_action == "run":
+        clear()
+        message("You ran away from the barbarian king, you proceed to the west.")
+        terrain = "mountain"
+        input("\nPress any key to continue...")
+    elif event_action == "surrender":
+        clear()
+        message("You surrendered to the barbarian king, he took all your items and you proceed to the east.")
+        items.clear()
+        terrain = "mountain"
+        input("\nPress any key to continue...")
+        clear()
+    elif event_action == "fight" and "sword" in items:
+        clear()
+        message("You fought the barbarian king and won, you proceed to the west.")
+        event_action = "east"
+        break
+    elif event_action == "fight" and "sword" not in items:
+        clear()
+        message("You fought the barbarian king and lost, you proceed to the north.")
+        event_action = "north"
+        break
+
+# Ending
+if terrain == "mountain" and event_action == "north":
+    message("You found a new city, you settled down. Game Over!")
+if terrain == "mountain" and event_action == "west" and "key" not in items:
+    message("You found a golden gate but could not unlock it you continued your journey. Game Over!")
+if terrain == "mountain" and event_action == "west" and "key" in items:
+    message("You found a golden gate and used the key to open it, you became the lord of the manor. Game Over!")
+if terrain == "river" and event_action == "east":
+    message("You became the barbarian king after defeating the previous leader. Game Over!")
+if terrain == "river" and event_action == "north" and "potion" not in items:
+    message("You suffered severe injury and died. Game Over!")
+if terrain == "river" and event_action == "north" and "potion" in items:
+    message("You suffered severe injury, but with the help of the potion you survived and continued your journey. Game Over!")
